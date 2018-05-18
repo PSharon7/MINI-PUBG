2a468ca35fcb9ce874286bdc9be72cdee801e648
Psharon7 <Psharon7@hotmail.com>
2018-05-18 15:55:23 +0800

    Occlusion

---
+++ Assets/Scripts/Occlusion/OcclusionObject.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OcclusionObject : MonoBehaviour
+{
+
+    Renderer myRend;
+    public float displayTime;
+
+    private void OnEnable()
+    {
+        myRend = gameObject.GetComponent<Renderer>();
+        displayTime = -1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (displayTime > 0)
+        {
+            displayTime -= Time.deltaTime;
+            myRend.enabled = true;
+        }
+        else
+        {
+            myRend.enabled = false;
+        }
+    }
+
+    public void HitOcclude(float time)
+    {
+        displayTime = time;
+        myRend.enabled = true;
+    }
+}
